85161cb89a2f73580b041ee80c168ac1f7ed95f2
Michael Collier <mcollier@microsoft.com>
2021-07-16 22:36:49 +0000

    Use UPN for role assignment.

--- scripts/my-permissions.sh
+++ scripts/my-permissions.sh
@@ -5,7 +5,7 @@ STORAGE_ACCOUNT_NAME=stqtk5kkh5hezug
 EVENT_HUB_NAMESPACE_NAME=evhns-qtk5kkh5hezug
 USER_PRINCIPAL_NAME=mcollier@microsoft.com
 
-assigneeId=$(az ad user list --upn $USER_PRINCIPAL_NAME --query []."objectId" -o tsv)
+# assigneeId=$(az ad user list --upn $USER_PRINCIPAL_NAME --query []."objectId" -o tsv)
 
 storageQueueRoleDefinitionId=$(az role definition list --name "Storage Queue Data Contributor" --query []."name" -o tsv)
 storageBlobRoleDefinitionId=$(az role definition list --name "Storage Blob Data Contributor" --query []."name" -o tsv)
@@ -14,6 +14,6 @@ eventHubRoleDefinitionId=$(az role definition list --name "Azure Event Hubs Data
 storageAccountResourceId=$(az storage account show --name $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP_NAME --query "id" -o tsv)
 eventHubResourceId=$(az eventhubs namespace show --name $EVENT_HUB_NAMESPACE_NAME -g $RESOURCE_GROUP_NAME --query "id" -o tsv)
 
-az role assignment create --assignee $assigneeId --role $storageQueueRoleDefinitionId --scope $storageAccountResourceId
-az role assignment create --assignee $assigneeId --role $storageBlobRoleDefinitionId --scope $storageAccountResourceId
-az role assignment create --assignee $assigneeId --role $eventHubRoleDefinitionId --scope $eventHubResourceId
+az role assignment create --assignee $USER_PRINCIPAL_NAME --role $storageQueueRoleDefinitionId --scope $storageAccountResourceId
+az role assignment create --assignee $USER_PRINCIPAL_NAME --role $storageBlobRoleDefinitionId --scope $storageAccountResourceId
+az role assignment create --assignee $USER_PRINCIPAL_NAME --role $eventHubRoleDefinitionId --scope $eventHubResourceId
